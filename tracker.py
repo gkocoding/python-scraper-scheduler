aa60ef710be91b4f117a9d67ff033f30e44ae48a
gkocoding <gosho.radew08@gmail.com>
2026-06-13 21:33:52 +0300

    JSON Added
    Save current prices to JSON

--- tracker.py
+++ tracker.py
@@ -1,8 +1,15 @@
 import requests
+import json
+
 
 response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=eur')
 data = response.json()
 ethereum_price = data["ethereum"]["eur"]
 bitcoin_price = data["bitcoin"]["eur"]
+previous_prices = {"bitcoin": bitcoin_price, "ethereum": ethereum_price}
+
+with open("previous_prices.json", "w") as file:
+    json.dump(previous_prices, file)
+
 print(f"Current Bitcoin price in EUR: {bitcoin_price}")
 print(f"Current Ethereum price in EUR: {ethereum_price}")
